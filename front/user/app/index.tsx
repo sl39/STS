4334338bbbecde7d5902d2244775d873f0a494b8
SewoolLee <swcl309@naver.com>
2024-09-09 19:00:24 +0900

    feat user login screen

--- front/user/app/index.tsx
+++ front/user/app/index.tsx
@@ -1,15 +1,45 @@
 import { Link, useRouter } from "expo-router";
-import { Text, View } from "react-native";
+import { useWindowDimensions, Text, View, StyleSheet } from "react-native";
+import { SocialLogin } from "../src/components/login";
 
 export default function index() {
   const router = useRouter();
+  const { height, width } = useWindowDimensions();
+
   const handleEnter = () => {
     router.push("/main");
   };
   return (
-    <View>
-      <Text> Login 화면 입니다</Text>
-      <button onClick={handleEnter}> main 화면으로 이동</button>
+    <View style={[style.container, { height: height, width: width }]}>
+      <View
+        style={[
+          style.view,
+          { height: height, width: width >= 768 ? 768 : width },
+        ]}
+      >
+        {/* <Text> Login 화면 입니다</Text>
+        <button
+          onClick={handleEnter}
+          style={{
+            width: width >= 768 ? 768 : width,
+          }}
+        >
+          main 화면으로 이동
+        </button> */}
+        <SocialLogin />
+      </View>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  container: {
+    backgroundColor: "#F2F2F2",
+    alignItems: "center",
+  },
+});
