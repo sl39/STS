c582cf6039310a3f97659f1b08cb231a618d2a1c
SewoolLee <swcl309@naver.com>
2024-09-23 18:40:38 +0900

    feat:

--- front/seller/src/components/signup/signup.tsx
+++ front/seller/src/components/signup/signup.tsx
@@ -9,25 +9,72 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { API_URL } from "@env";
+import axios from "axios";
+import BankListScreen from "./bankListScreen";
+
+type bankType = {
+  label: string;
+  value: string;
+};
 
 export function Signup() {
-  const [sellerId, setSellerId] = useState("");
+  const [email, setSellerId] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [bankAccount, setBankAccount] = useState("");
   const [businessNumber, setBusinessNumber] = useState("");
-  const [storePhone, setStorePhone] = useState("");
-  const [sellerName, setSellerName] = useState("");
-
+  const [ownerPhone, setStorePhone] = useState("");
+  const [ownerName, setSellerName] = useState("");
+  const [confirmOwnerPhone, setConfirmOwnerPhone] = useState<boolean>(true);
+  const [confirmBusinessNumber, setConfirmBusinessNumber] =
+    useState<boolean>(true);
+  const [bankName, setBankName] = useState<bankType | null>(null);
+  const handleBank = (value: bankType) => {
+    setBankName(value);
+  };
   const router = useRouter();
 
   const handleSignup = () => {
-    router.push("/signup/store");
+    if (email === "") {
+      alert("이메일을 입력해 주세요");
+      return;
+    }
+    if (password === "") {
+      alert("비밀번호를 입력해 주세요");
+      return;
+    }
+    if (confirmPassword !== password) {
+      alert("비밀번호를 동일하게 입력해 주세요");
+      return;
+    }
+    if (bankAccount === "") {
+      alert("계좌번호를 입력해 주세요");
+      return;
+    }
+    if (ownerPhone === "" || !confirmOwnerPhone) {
+      alert("전화번호를 인증해주세요");
+      return;
+    }
+    if (!confirmBusinessNumber) {
+      alert("사업자 번호를 인증해주세요");
+      return;
+    }
+
+    if (ownerName === "") {
+      alert("이름을 입력해 주세요");
+      return;
+    }
+    if (!bankName) {
+      alert("은행을 선택해주세요");
+      return;
+    }
+    // router.push("/signup/store");
   };
 
   // 판매자 id
   const idConfirm = () => {
-    console.log(sellerId);
+    console.log(email);
   };
 
   // 정산 입금 계좌번호
@@ -38,10 +85,30 @@ export function Signup() {
   // 사업자 번호 확인
   const bnConfirm = () => {
     console.log(businessNumber);
+    const validBusinessNumber = async () => {
+      // const header = { "Content-Type": "application/json" };
+      const url = API_URL + "/api/auth/owner/brn";
+      const val = {
+        brn: businessNumber,
+      };
+      try {
+        const data = await axios.post(url, val);
+        if (data.data.valid) {
+          setConfirmBusinessNumber(true);
+          alert("사업자 번호가 인증되었습니다");
+        } else {
+          setConfirmBusinessNumber(false);
+          alert("사업자 번호인증에 실패하였습니다");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    validBusinessNumber();
   };
 
   const storePhoneConfirm = () => {
-    console.log(storePhone);
+    console.log(ownerPhone);
   };
 
   return (
@@ -52,7 +119,7 @@ export function Signup() {
           <TextInput
             style={styles.specInput}
             placeholder="판매자 아이디"
-            value={sellerId}
+            value={email}
             onChangeText={setSellerId}
             keyboardType="number-pad"
           />
@@ -67,7 +134,9 @@ export function Signup() {
             style={styles.specInput}
             placeholder="사업자 번호"
             value={businessNumber}
-            onChangeText={setBusinessNumber}
+            onChangeText={(text) => {
+              setBusinessNumber(text), setConfirmBusinessNumber(false);
+            }}
             keyboardType="number-pad"
           />
           <TouchableOpacity style={styles.specBtn} onPress={bnConfirm}>
@@ -80,7 +149,7 @@ export function Signup() {
           <TextInput
             style={styles.specInput}
             placeholder="판매자 전화번호"
-            value={storePhone}
+            value={ownerPhone}
             onChangeText={setStorePhone}
             keyboardType="number-pad"
           />
@@ -93,7 +162,7 @@ export function Signup() {
         <TextInput
           style={styles.input}
           placeholder="판매자 이름"
-          value={sellerName}
+          value={ownerName}
           onChangeText={setSellerName}
         />
         <TextInput
@@ -110,7 +179,13 @@ export function Signup() {
           onChangeText={setConfirmPassword}
           secureTextEntry
         />
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <TextInput
             style={styles.specInput}
             placeholder="정산입금계좌번호"
@@ -118,6 +193,7 @@ export function Signup() {
             onChangeText={setBankAccount}
             keyboardType="number-pad"
           />
+          <BankListScreen handleBank={handleBank} />
           <TouchableOpacity style={styles.specBtn} onPress={accountConfirm}>
             <Text style={{ color: "#3498db", fontWeight: "bold" }}>
               인증하기
@@ -173,6 +249,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignItems: "center",
     justifyContent: "center",
+    zIndex: -1,
   },
   buttonText: {
     color: "white",
