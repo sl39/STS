5a6dfe3ae04a59c93a8ec5a7d34067eda4e34f06
hyobin0704 <jhb46620@gmail.com>
2024-09-24 16:33:52 +0900

    feat: 메뉴이름 검색

--- back/src/main/java/org/ex/back/domain/owner/controller/OwnerController.java
+++ back/src/main/java/org/ex/back/domain/owner/controller/OwnerController.java
@@ -33,8 +33,8 @@ public class OwnerController {
     @Value("${openApi.serviceKey}")
     private String openApiKey;
 
-    @Value("${portone.api.key}")
-    private String portoneApiKey;
+    //@Value("${portone.api.key}")
+    //private String portoneApiKey;
 
     private final OwnerService ownerService;
 
